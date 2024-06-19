d67bd353af7cb3d35fbc120737dad1c807c8d0f4
Sam <sammatanz@gmail.com>
2024-06-20 10:34:19 +1200

    Removed Redundant Imports

--- scripts/model_testing.py
+++ scripts/model_testing.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import random
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
